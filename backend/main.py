ba8ef1fefcc1ed7cb5ea9ee1bb247dfb8eedc191
tangke <tangketk@foxmail.com>
2026-04-05 21:19:47 +0800

    feat: configure VITE_BASE_URL for frontend and update request handling

--- backend/main.py
+++ backend/main.py
@@ -15,4 +15,4 @@ app.add_middleware(
 
 app.include_router(stock_router, prefix="/api/stocks", tags=["Stocks Market"])
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=18000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=18000, reload=True, reload_dirs=["app"])
